15b0075a58a43c69cd1aa609cc7814e0807d037b
Vikash Kumar <vikash9932@gmail.com>
2022-08-14 19:07:00 +0530

    feat: login, signup & Navbar implementation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Signup from './pages/signup/Signup'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//pages & components
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='app'>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,10 +1,47 @@
-import styles from './Signup.module.css'
-const Signup = () => {
-    return (
-        <div>
-            Signup
-        </div>
-    )
-}
+import { useState } from 'react';
 
-export default Signup
+//styles
+import styles from './Signup.module.css';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Signup</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          type='text'
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
+  );
+};
+
+export default Signup;
